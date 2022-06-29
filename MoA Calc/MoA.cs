22044186d005e7ec32c14ef1f6bb3ec8eb3eb2ed
Cameron Hickey <cam@macstir.com>
2022-06-28 20:39:04 -0700

    1st official update going out

--- MoA Calc/MoA.cs
+++ MoA Calc/MoA.cs
@@ -8,16 +8,43 @@ namespace MoA_Calc
 {
     class MoA
     {
-        public static void MoAMath(double num1, double num2, double num3)
+        double num1, num2, num3;
+        public MoA() // Default Constructor
+        {
+            double num1 = 0.0;
+            double num2 = 0.0;
+            double num3 = 0.0;
+        }
+        public MoA(double num1, double num2, double num3) // Parametric Constructor
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        public double[] MoAMath(double num1, double num2, double num3) // Math operators for inches per MOA, MOA Degree Adjustment, Clicks required on scope 
         {
             double moainch = num1 / (100);
             double moaAdjust = num2 / moainch;
+
             double moaScope = num3 / moaAdjust;
 
-            Console.WriteLine($"Inches per MOA at {num1} is {moainch} inches.");
-            Console.WriteLine($"MOA adjustment is {moaAdjust}");
-            Console.WriteLine($"Number of clicks for scope adjustment is {moaScope}");
+            double[] MinuteAngle = new double[3];
+            MinuteAngle[0] = moainch;
+            MinuteAngle[1] = moaAdjust;
+            MinuteAngle[2] = moaScope;
 
+            return MinuteAngle;
         }
+        public static void PrintMoa(double[] Minute) // Prints the MoA table
+        {       
+            Console.WriteLine($"\t Inches per MOA at that distance : {Minute[0]} ");
+            Console.WriteLine($"\t MOA adjustment : {Minute[1]} Inches");
+            if (!(Minute[2] > 0) & !(Minute[2] < 0))
+            {
+                Console.WriteLine("No adjustment is needed.");     // Quick catch case if adjustment is 0
+            }
+            else Console.WriteLine($"\t Adjustment in clicks on scope : {Minute[2]} Clicks");
+        }
+        
     }
 }
--- MoA Calc/Program.cs
+++ MoA Calc/Program.cs
@@ -5,9 +5,12 @@
         static void Main(string[] args)
         {
             bool endApp = false;
-            // Display title as the C# console calculator app.
+            // Display title.
             Console.WriteLine("Minute of Angle Conversion\r");
+            Console.WriteLine();
+            Console.WriteLine("For scope adjustment.");
             Console.WriteLine("------------------------\n");
+
 
             while (!endApp)
             {
@@ -16,45 +19,45 @@
                 string numInput2 = "";
                 string numInput3 = "";
 
-                // Ask the user to type the first number.
+                // Ask the user to type Distance.
                 Console.Write("Enter Distance to target in yards: ");
                 numInput1 = Console.ReadLine();
 
-                double cleanNum1 = 0;
-                while (!double.TryParse(numInput1, out cleanNum1))
+                double testNum1 = 0;
+                while (!double.TryParse(numInput1, out testNum1))
                 {
                     Console.Write("This is not valid input. Please enter an integer or double value: ");
                     numInput1 = Console.ReadLine();
                 }
 
-                // Ask the user to type the second number.
-                Console.WriteLine("Enter number of inches of adjustment needed: ");
+                // Ask the user to type Inches of adjustment.
+                Console.WriteLine("Enter number of inches of adjustment needed (Use Negative for left) : ");
                 numInput2 = Console.ReadLine();
 
-                double cleanNum2 = 0;
-                while (!double.TryParse(numInput2, out cleanNum2))
+                double testNum2 = 0;
+                while (!double.TryParse(numInput2, out testNum2))
                 {
                     Console.Write("This is not valid input. Please enter an integer value: ");
                     numInput2 = Console.ReadLine();
                 }
-                
+                // Ask user to input clicks per MOA on their scope
                 Console.Write("Enter number of clicks per 1 MOA on scope: ");
                 numInput3 = Console.ReadLine();
 
-                double cleanNum3 = 0;
-                while (!double.TryParse(numInput3, out cleanNum3))
+                double testNum3 = 0;
+                while (!double.TryParse(numInput3, out testNum3))
                 {
                     Console.Write("This is not valid input. Please enter an integer or double value: ");
                     numInput3 = Console.ReadLine();
                 }
-                MoA.MoAMath(cleanNum1, cleanNum2, cleanNum3);
-                
+                MoA moA = new MoA(testNum1,testNum2,testNum3);
+                MoA.PrintMoa(moA.MoAMath(testNum1, testNum2, testNum3));
 
                 Console.WriteLine("------------------------\n");
 
                 // Wait for the user to respond before closing.
-                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
-                if (Console.ReadLine() == "n") endApp = true;
+                Console.Write("Press 'Q' and Enter to close the app, or press any other key and Enter to continue: ");
+                if (Console.ReadLine() == "Q") endApp = true;
 
                 Console.WriteLine("\n"); // Friendly linespacing.
             }
